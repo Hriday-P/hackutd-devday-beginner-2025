23e78f66c3d9ce0a84dc011b307e4ead82bcb205
Hriday Patel <hridaypatel@Hridays-MacBook-Pro.local>
2025-09-13 17:08:57 -0500

    Initial

--- src/app/components/hero.js
+++ src/app/components/hero.js
@@ -2,8 +2,10 @@ import React from 'react'
 
 function Hero() {
   return (
-    <div>
-
+    <div className="flex justify-center py-16 px-8 bg-blue-100 border-b border-gray-300 text-black">
+      <div>
+        Maxing our GPA, One Class at a Time
+      </div>
     </div>
   );
 }
--- src/app/components/navbar.js
+++ src/app/components/navbar.js
@@ -1,12 +1,28 @@
 import React from "react";
 import Link from "next/link";
-import { User, Settings } from "lucide-react";
+import { User, Settings, Aperture } from "lucide-react";
 
 function Navbar() {
     return (
-        <div>
-            
-        </div>
+        <nav className="flex justify-between items-center py-4 px-8 bg-gray-100 border-b border-gray-300 text-black">
+            <div className="flex gap-4">
+                <Link href="/settings" className="group relative">
+                    <Settings size= {24} className="cursor-pointer"/>
+                </Link>
+            </div>
+
+            <div className="font-bold test-3xl"/>
+            <Link href="/" className="group relative">
+                <span className="cursor-pointer">DevGPA</span>
+                </Link>
+            <div/>
+            <div className="flex gap-4">
+                <Link href="/profile" className="group relative">
+                <User size={24} className="cursor-pointer"/>
+                </Link>
+            </div>
+
+        </nav>
     );
 }
 
